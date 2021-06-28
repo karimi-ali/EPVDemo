8704f59e1fb6ecf23100b88ff5240c0863416da6
Ali Karimi <karimi.k.ali@gmail.com>
2021-06-28 13:47:06 +0200

    before running it further

--- code/Script.R
+++ code/Script.R
@@ -1,19 +1,22 @@
 ## Script based on the pdf tutorial
 
 # directories
+setwd("~/code/basketball/EPVDemo")
 code.dir <- "./code"
 data.dir <- "./data"
 
 # full game of optical tracking data
 dat <- read.csv(file=sprintf("%s/2013_11_01_MIA_BKN.csv", data.dir))
 
-# Draw a specifc time point
-source(sprintf("%s/constants.R", code.dir)) # loads libraries and constants used throughout code
-source(sprintf("%s/graphics.R", code.dir)) # graphics/plotting functions
-par(mar=c(0, 0, 0, 0))
+# Draw a specific time point
+# loads libraries and constants used throughout code
+source("./code/constants.R")
+# graphics/plotting functions
+source("./code/graphics.R")
+par(mfrow=c(1,1))
 data.plotter(dat, 1800)
 
-# Load precomputed data
+# Load pre-computed data
 load(sprintf("%s/tdat.Rdata", data.dir))
 
 # Player similarity matrix: H
@@ -23,6 +26,18 @@ head(players)
 
 # Plot occupancy matrix rows
 par(mfrow=c(1,5))
+
+
+source(sprintf("%s/data_formatting.R", code.dir))
+source(sprintf("%s/covariates.R", code.dir))
+poss <- possession.indicator(dat) # infer ballcarrier... takes about a minute
+tdat <- rearrange.data(dat, poss) # re-shuffle columns by to ballcarrier... (2 min)
+tdat <- offensive.halfcourt(tdat) # transforming to offensive halfcourt
+tdat <- offensive.ballcarrier(tdat)
+touchID <- get.touchID(tdat)
+covariates <- getAllCovars(tdat) # get covariates... (3 min)
+tdat <- data.frame(tdat, touchID=touchID, covariates)
+save(tdat, file=sprintf("%s/tdat.Rdata", data.dir))
 for(i in 1:5)
   spatialPlot0(df[i, ], legend=F)
 
