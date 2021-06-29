e4ace85fdfe2d8adb960f02e2dc9bac8c2371f7a
Ali Karimi <karimi.k.ali@gmail.com>
2021-06-29 14:44:53 +0200

    start understanding the INLA package

--- code/Script.R
+++ code/Script.R
@@ -6,14 +6,14 @@ code.dir <- "./code"
 data.dir <- "./data"
 
 # full game of optical tracking data
-dat <- read.csv(file=sprintf("%s/2013_11_01_MIA_BKN.csv", data.dir))
+dat <- read.csv(file = sprintf("%s/2013_11_01_MIA_BKN.csv", data.dir))
 
 # Draw a specific time point
 # loads libraries and constants used throughout code
 source("./code/constants.R")
 # graphics/plotting functions
 source("./code/graphics.R")
-par(mfrow=c(1,1))
+par(mfrow = c(1, 1))
 data.plotter(dat, 1800)
 
 # Load pre-computed data
@@ -25,7 +25,7 @@ players <- read.csv(sprintf("%s/players2013.csv", data.dir))
 head(players)
 
 # Plot occupancy matrix rows
-par(mfrow=c(1,5))
+par(mfrow = c(1, 5))
 
 
 source(sprintf("%s/data_formatting.R", code.dir))
@@ -34,11 +34,11 @@ poss <- possession.indicator(dat) # infer ballcarrier... takes about a minute
 tdat <- rearrange.data(dat, poss) # re-shuffle columns by to ballcarrier... (2 min)
 tdat <- offensive.halfcourt(tdat) # transforming to offensive halfcourt
 tdat <- offensive.ballcarrier(tdat)
-touchID <- get.touchID(tdat)
+touch_ID <- get.touchID(tdat)
 covariates <- getAllCovars(tdat) # get covariates... (3 min)
-tdat <- data.frame(tdat, touchID=touchID, covariates)
-save(tdat, file=sprintf("%s/tdat.Rdata", data.dir))
-for(i in 1:5)
-  spatialPlot0(df[i, ], legend=F)
+tdat <- data.frame(tdat, touchID = touch_ID, covariates)
+save(tdat, file = sprintf("%s/tdat.Rdata", data.dir))
+for (i in 1:5)
+  spatialPlot0(df[i, ], legend = F)
 
 # Do the rest when the new data is put into a database
---
+++ code/test_inla.R
@@ -0,0 +1,17 @@
+## The script for testing the INLA package
+library(INLA)
+
+n = 100; a = 1; b = 20; tau = 100
+z = rnorm(n)
+eta = a + b*z
+
+scale = exp(rnorm(n))
+prec = scale*tau
+y = rnorm(n, mean = eta, sd = 1/sqrt(prec))
+
+
+data = list(y = y, z = z)
+formula = y ~ 1 + z
+result = inla(formula, family = "gaussian", data = data)
+
+print(summary(result))
